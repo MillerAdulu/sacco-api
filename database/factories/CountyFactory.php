d05f39ea7ebf49c0a3a8b46646ebb264d516b86f
Miller Adulu <milleradulu@gmail.com>
2018-06-23 09:22:44 +0300

    County seeding and factory implementation

--- database/factories/CountyFactory.php
+++ database/factories/CountyFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\County::class, function (Faker $faker) {
     return [
-        //
+        'county_code' => $faker->unique()->stateAbbr,
+        'county_name' => $faker->word
     ];
 });

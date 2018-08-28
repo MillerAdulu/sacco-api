7a9114be9e6aa575ac39670c42d6b6e20dbb3a36
Miller Adulu <milleradulu@gmail.com>
2018-08-28 15:51:06 +0300

    Error and email resources

---
+++ resources/views/email/account.blade.php
@@ -0,0 +1,6 @@
+<div>
+  Name: {{ $member->last_name}}, {{ $member->first_name }} {{ $member->other_name }} <br>
+  Phone: {{ $member->phone_number}} <br>
+  Email: {{ $member->email }} <br>
+  Password: {{ $member->password }}
+</div>

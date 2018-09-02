08203b0a611c2f306e3219585c928b784db6530b
Miller Adulu <milleradulu@gmail.com>
2018-09-02 19:40:35 +0300

    Add login link to email

--- resources/views/email/account.blade.php
+++ resources/views/email/account.blade.php
@@ -1,4 +1,5 @@
 <div>
+  Click on this link to log in to your account: <a href="https://sedcadmin.herokuapp.com">https://sedcadmin.herokuapp.com</a> <br>
   Name: {{ $member->last_name}}, {{ $member->first_name }} {{ $member->other_name }} <br>
   Phone: {{ $member->phone_number}} <br>
   Email: {{ $member->email }} <br>

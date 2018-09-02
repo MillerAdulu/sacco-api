ffabb3063ccd6de82f716e581655daebaf600e28
Miller Adulu <milleradulu@gmail.com>
2018-09-02 19:27:33 +0300

    Return error on user not found

--- app/Http/Controllers/LoginUserController.php
+++ app/Http/Controllers/LoginUserController.php
@@ -1,24 +1,26 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use App\Http\Resources\UserResource;
-use App\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-
-class LoginUserController extends Controller
-{
+  
+  namespace App\Http\Controllers;
+  
+  use App\Http\Resources\UserResource;
+  use App\User;
+  use Illuminate\Http\Request;
+  use Illuminate\Support\Facades\Hash;
+  
+  class LoginUserController extends Controller
+  {
     public function login(Request $request)
     {
-        $user = User::where('email', $request->username)
-            ->orWhere('phone_number', $request->username)->first();
-
-        if (Hash::check($request->password, $user->password)) {
-            return new UserResource(
-                $user
-            );
-        }
-        return response()->json(['error' => "Not Password"], 500);
+      $user = User::where('email', $request->username)
+        ->orWhere('phone_number', $request->username)->first();
+      
+      if(!$user) return response()->json(['No such user!'], 200);
+      
+      if (Hash::check($request->password, $user->password)) {
+        return new UserResource(
+          $user
+        );
+      }
+      return response()->json(['error' => "Wrong Password"], 200);
     }
-}
+  }

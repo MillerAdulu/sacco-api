0415c8ccc0147c4f349cca7dda12ec0cc658636b
Miller Adulu <milleradulu@gmail.com>
2018-09-05 12:55:36 +0300

    Removed passport providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Support\Facades\Schema;
-// use Illuminate\Support\ServiceProvider;
-
-use Laravel\Passport\Passport;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +18,6 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         
         Resource::withoutWrapping();
-
-        $this->registerPolicies();
     }
 
     /**

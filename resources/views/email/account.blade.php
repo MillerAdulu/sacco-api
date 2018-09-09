a336a476b667cc3d4a416c26ffed85e57460ce39
Miller Adulu <milleradulu@gmail.com>
2018-09-09 22:58:50 +0300

    Setup new url to login after registration

--- resources/views/email/account.blade.php
+++ resources/views/email/account.blade.php
@@ -1,5 +1,5 @@
 <div>
-  Click on this link to log in to your account: <a href="https://sedcadmin.herokuapp.com">https://sedcadmin.herokuapp.com</a> <br>
+  Click on this link to log in to your account: <a href="https://elegant-panini-9bc637.netlify.com">https://elegant-panini-9bc637.netlify.com</a> <br>
   Name: {{ $member->last_name}}, {{ $member->first_name }} {{ $member->other_name }} <br>
   Phone: {{ $member->phone_number}} <br>
   Email: {{ $member->email }} <br>
